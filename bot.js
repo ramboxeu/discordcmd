e714e0e652a6dc78d547fed8e8d91cdd5e5d2457
RamboxEu <mail.rambox@gmail.com>
2018-03-21 13:45:50 +0100

    New commads parser & Issue #1

--- bot.js
+++ bot.js
@@ -1,12 +1,22 @@
 const Discord = require(`discord.js`)
-const config = require(`./config.json`)
 
 const client = new Discord.Client();
+
+const hellos = [`Yo`, `Hello`, `Hi`, `Whats up`, `Greetings`];
+
+const commands = {
+    ping: function (...data){
+        return `:ping_pong: Pong!`;
+    },
+    hello: function (...data){
+        return `${hellos[Math.round((Math.random() * 4) + 1)]} ${data[0].author.username}!`;
+    }
+}
 
 client.on(`ready`, () => {
     console.log(`Welcome Discord users!`);
 
-    client.user.setActivity(`Version: 1.0.0`);
+    client.user.setActivity(`Version: 0.1`);
 })
 
 client.on(`message`, (message) => {
@@ -16,15 +26,7 @@ client.on(`message`, (message) => {
     const args = message.content.slice(1).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    switch(command){
-        case `ping`:
-            message.channel.send(`> :ping_pong: Pong!`);
-            break;
-        case `hello`:
-            const hellos = [`Yo`, `Hello`, `Hi`, `Whats up`, `Greetings`];
-            message.channel.send(`${hellos[Math.round((Math.random() * 4) + 1)]} ${message.author.username}!`);
-            break;
-    }
+    message.channel.send(commands[command](message, args));
 })
 
 // I'm using config vars in Heroku to teek my token secret
